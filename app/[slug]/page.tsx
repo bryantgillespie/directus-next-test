03098a6ee331d058c729730e23676503f0a34536
bryantgillespie <hey@bryantgillespie.com>
2023-05-08 09:04:41 -0400

    generateStaticParams

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,18 @@ import directus from '../../lib/directus';
 import PageBuilder from '../../components/page-builder';
 import { notFound } from 'next/navigation';
 
+export async function generateStaticParams() {
+    const { data: pages } = await directus.items('pages').readByQuery({
+        fields: ['slug'],
+    });
+
+    return pages.map((page) => ({
+        params: {
+            slug: page.slug,
+        },
+    }));
+}
+
 async function getPage(slug) {
 	try {
 		const { data: pages} = await directus.items('pages').readByQuery({
@@ -20,7 +32,7 @@ async function getPage(slug) {
 	}
 }
 
-export default async function DynamicPage({ params }) {
+export default async function DynamicPage({ params } : { params: { slug: string } }) {
 	const page = await getPage(params.slug);
 	return (
 		<div>
